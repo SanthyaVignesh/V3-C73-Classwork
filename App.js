3677909c77863649d0118e41ac3893c1f3d6bedf
SanthyaVignesh <santhyasvignesh@gmail.com>
2021-09-05 22:32:37 +0530

    C70 Addition

--- App.js
+++ App.js
@@ -2,13 +2,45 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import BottomNavigatorTab  from './components/BottomNavigatorTab';
+import * as Font from "expo-font";
+import {Oldenburg_400Regular } from '@expo-google-fonts/oldenburg';
+
+
 
 export default class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      fontLoaded :false
+    }
+  }
+
   render(){
-    return <BottomNavigatorTab/>
+    const {fontLoaded} = this.state;
+    if(fontLoaded){
+      return <BottomNavigatorTab/>
+    }
+    return null;
+  }
+
+  async loadFonts(){
+      await Font.loadAsync({
+        Oldenburg_400Regular : Oldenburg_400Regular
+      });
+
+      this.setState({
+        fontLoaded : true
+      })
+  }
+  
+  componentDidMount(){
+    this.loadFonts();
   }
   
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
--- Screens/TransactionScreen.js
+++ Screens/TransactionScreen.js
@@ -1,7 +1,11 @@
 import * as React from 'react';
-import {Text,View,StyleSheet, TouchableOpacity} from 'react-native';
+import {Text,View,StyleSheet,TextInput, TouchableOpacity, ImageBackground ,Image} from 'react-native';
 import * as Permissions from 'expo-permissions';
 import { BarCodeScanner } from 'expo-barcode-scanner';
+
+const bgImg = require("../assets/images/background2.png");
+const appIcon = require("../assets/images/appIcon.png");
+const appName = require("../assets/images/appName.png");
 
 
 export default class TransactionScreen extends React.Component{
@@ -11,7 +15,8 @@ export default class TransactionScreen extends React.Component{
             domState : "normal",
             hasCamPermission : null,
             scanned : false,
-            scannedData : ""
+            bookId : "",
+            studentId : ""
         };
     }
 
@@ -26,18 +31,30 @@ export default class TransactionScreen extends React.Component{
     }
 
     handleBarCodeScanned = async ({type,data}) =>{
-        this.setState ({
-            scannedData : data,
-            domState : "normal",
-            scanned : true
-        })
+        const {domState} = this.state;
+
+        if(domState === "bookId"){
+            this.setState ({
+                bookId : data,
+                domState : "normal",
+                scanned : true
+            })
+        }
+        else if(domState === "studentId"){
+            this.setState ({
+                studentId : data,
+                domState : "normal",
+                scanned : true
+            })
+        }
+       
     }
 
     
 
     render(){
-        const {domState,hasCamPermission,scanned,scannedData} = this.state;
-        if(domState === "scanner"){
+        const {domState,hasCamPermission,scanned,bookId,studentId} = this.state;
+        if(domState !== "normal"){
             return(
                 <BarCodeScanner onBarCodeScanned = {scanned ? undefined : this.handleBarCodeScanned}
                 style={StyleSheet.absoluteFillObject}/>
@@ -45,16 +62,48 @@ export default class TransactionScreen extends React.Component{
         }
         else{
             return(
+                
                 <View style = {styles.viewContainer}>
-                    <Text style = {styles.textStyle}>
-                        {hasCamPermission ? scannedData : "Request Camera Permission"}
-                    </Text>
-                    <TouchableOpacity style= { styles.button } onPress = {()=>{this.getCameraPermission("scanner")}}>
-                        <Text style = {styles.textStyle}>
-                            Scan QR Code
-                        </Text>
-                    </TouchableOpacity>
-            </View>
+                    <ImageBackground source = {bgImg} style = {styles.bgImage}>
+                    <View  style = {styles.upperContainer}>
+                        <Image source = {appIcon} style = {styles.appIcon}></Image>
+                        <Image source = {appName} style = {styles.appName}></Image>
+                    </View>
+                    <View style = {styles.lowerContainer}>
+                        <View style = {styles.textinputContainer}>
+                            <TextInput 
+                                style = {styles.textStyle}
+                                placeholder = {"Book id"}
+                                placeholderTextColor = {"#FFFFFF"}
+                                value = {bookId}
+                            />
+                            <TouchableOpacity 
+                                style = {styles.button}
+                                onPress = {()=>{
+                                    this.getCameraPermission("bookId")
+                                }}>
+                                    <Text style= {styles.buttonText}>Scan</Text>
+                            </TouchableOpacity>
+                        </View>
+                        <View style = {[styles.textinputContainer, {marginTop : 25}]} >
+                        <TextInput 
+                                style = {styles.textStyle}
+                                placeholder = {"Student id"}
+                                placeholderTextColor = {"#FFFFFF"}
+                                value = {studentId}
+                            />
+                            <TouchableOpacity 
+                                style = {styles.button}
+                                onPress = {()=>{
+                                    this.getCameraPermission("studentId")
+                                }}>
+                                    <Text style= {styles.buttonText}>Scan</Text>
+                            </TouchableOpacity>
+
+                        </View>
+                    </View>  
+                    </ImageBackground> 
+                </View>
             );
     
         }
@@ -62,15 +111,55 @@ export default class TransactionScreen extends React.Component{
 }
 
 const styles =  StyleSheet.create({
+    bgImage :{
+        flex : 1,
+        resizeMode : "cover",
+        justifyContent :"center"
+    },
+    upperContainer:{    
+        flex : 0.5,
+        justifyContent : "center",
+        alignItems : "center"
+    },
+    appIcon:{
+        width : 200,
+        height : 200,
+        resizeMode : "contain",
+        marginTop : 80
+    },
+    appName :{
+        width : 80,
+        height : 80,
+        resizeMode : "contain"
+    },
     viewContainer : {
         flex : 1,
         justifyContent : "center",
         alignItems  : "center",
         backgroundColor :  "#5653D4"
     },
+    lowerContainer :{
+        flex : 0.5,
+        alignItems : "center"
+    },
+    textinputContainer : {
+        borderWidth :2,
+        borderRadius : 10,
+        flexDirection : "row",
+        backgroundColor : "#9DFD24",
+        borderColor : "#FFFFFF"
+    },
     textStyle : {   
         color : "#ffffff",
-        fontSize : 30
+        fontSize : 18,
+        fontFamily : "Oldenburg_400Regular",
+        width : "57%",
+        height : 50,
+        padding :10,
+        borderColor : "#FFFFFF",
+        borderRadius : 10,
+        borderWidth : 3,
+
     },
     button :{
         width : "43%",
@@ -82,6 +171,7 @@ const styles =  StyleSheet.create({
     },
     buttonText : {
         fontSize : 24,
+        fontFamily : "Oldenburg_400Regular",
         color : "#FFFFFF"
     }
 })
